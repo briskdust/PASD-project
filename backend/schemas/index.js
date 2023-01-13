a21c72dd716925cd2b6b141fdb4ca7f125a10914
oscardef <o.m.de.francesca@student.rug.nl>
2023-01-13 13:58:15 +0100

    Delivery schema

--- backend/schemas/index.js
+++ backend/schemas/index.js
@@ -1,4 +1,5 @@
 import order from "./order"
 import user from "./user"
+import delivery from "./delivery"
 
-export const schemaTypes = [order, user]
+export const schemaTypes = [order, user, delivery]
