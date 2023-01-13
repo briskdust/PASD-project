6bb7c40fa26f85752b325201dfe3320f77ff39f5
briskdust <briskdust@gmail.com>
2023-01-13 12:58:34 +0100

    Implement add and fetch functionality

---
+++ src/client.js
@@ -0,0 +1,10 @@
+import sanityClient from "@sanity/client";
+
+export default sanityClient({
+  projectId: "5fkrgo69",
+  dataset: "production",
+  useCdn: true,
+  withCredentials: true,
+  token:
+    "skSjgyHOQT4NUYTkXBoZViogQDFZqIhaLZKBktBmQigFmBiS1MA6zNG0KaVl8WnfApB5THKVbfu9DhLslIyGUWEjDOb2ptny5cGnilENi7M6RVenfq6ezDfWGRkrat7Gd7f5aNJwg8GqxcqygC3cOUXyEG6R99AyxbMwY5hIgdpM7OsGzMO3",
+});
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,21 @@
 import "./temp.css";
+import sanityClient from "../client";
 
 const HomePage = () => {
-  return;
+  const doc = {
+    _type: "order",
+    name: "briskdust",
+  };
+
+  sanityClient
+    .fetch(`*[_type == "order"]`)
+    .then(data => console.log(data))
+    .catch(console.error);
+
+  const trialPatch = () => {
+    sanityClient.create(doc);
+  };
+  return <button onClick={trialPatch}></button>;
 };
 
 export default HomePage;
