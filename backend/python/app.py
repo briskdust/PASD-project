cc7eac1386756c93a57cbab98b72f5443524e830
briskdust <briskdust@gmail.com>
2023-01-16 20:36:12 +0100

    Implement python backend

--- backend/python/app.py
+++ backend/python/app.py
@@ -3,6 +3,8 @@ import requests
 from flask import Flask, request
 from flask_cors import CORS
 
+label_file = {'labelFile': open('Fragile.pdf', 'rb')}
+
 app = Flask(__name__)
 CORS(app)
 
@@ -14,16 +16,59 @@ def get_orders():
     return r.json()
 
 
-@app.post("/deliveries")
-def post_deliveries():
+@app.post("/delivery")
+def post_delivery():
     data = request.get_json()
-    print(data)
-    r = requests.post("https://pasd-webshop-api.onrender.com/api/delivery",
-                      headers={"x-api-key": "hBqPEeyJNXexUZfgJRCZ"}, json=data)
 
-    res = r.json()
+    r = requests.post("https://pasd-webshop-api.onrender.com/api/delivery/",
+                      headers={"x-api-key": "hBqPEeyJNXexUZfgJRCZ",
+                               "Content-Type": "application/json"},
+                      json=data)
 
-    return res, 204
+    print(r.status_code)
+
+    if r.status_code == 200:
+        return r.json()
+    else:
+        return "", r.status_code
+
+
+@app.get("/label/<delivery_id>")
+def add_label(delivery_id):
+    url = f"https://pasd-webshop-api.onrender.com/api/label?delivery_id={delivery_id}"
+    r = requests.post(url,
+                      headers={"x-api-key": "hBqPEeyJNXexUZfgJRCZ"},
+                      files=label_file)
+
+    if r.status_code == 200:
+        return "", 200
+    else:
+        return r.json()
+
+
+@app.get("/pickup/<delivery_id>")
+def pickup(delivery_id):
+    url = f"https://pasd-webshop-api.onrender.com/api/delivery/{delivery_id}"
+    r = requests.patch(url, headers={"x-api-key": "hBqPEeyJNXexUZfgJRCZ"},
+                       json={"status": "TRN"})
+
+    if r.status_code == 200:
+        return "", 200
+    else:
+        return r.json()
+
+
+@app.get("/delivered/<delivery_id>")
+def delivered(delivery_id):
+    url = f"https://pasd-webshop-api.onrender.com/api/delivery/{delivery_id}"
+    r = requests.patch(url, headers={"x-api-key": "hBqPEeyJNXexUZfgJRCZ"},
+                       json={"status": "DEL",
+                             "actual_deliver_datetime": "2022-12-08T08:34:29.076Z"})
+
+    if r.status_code == 200:
+        return "", 200
+    else:
+        return r.json()
 
 
 if __name__ == '__main__':
--- frontend/src/components/RegisterDelivery.jsx
+++ frontend/src/components/RegisterDelivery.jsx
@@ -1,51 +1,69 @@
 import axios from "axios";
 import sanityClient from "../client";
-
-const added = [];
+import { useState } from "react";
 
 const RegisterDelivery = () => {
-  const options = {
-    method: "POST",
-    url: "http://localhost:5000/deliveries",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-
   let currentTime = new Date().getTime();
   let updatedTIme = new Date(currentTime + 2 * 60 * 60 * 1000);
   const newTime = updatedTIme.toISOString();
+  const [orders, setOrders] = useState([]);
 
-  const registerAllOrders = () => {
-    sanityClient
-      .fetch(`*[_type == "order"]`)
-      .then(data => {
-        console.log(data);
-        for (const order of data) {
-          const doc = {
-            order_id: order.id,
-            price_in_cents: 0,
-            expected_delivery_datetime: newTime,
-          };
-
-          if (added.includes(order)) {
-            continue;
-          } else {
-            axios({
-              url: "http://localhost:5000/deliveries",
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              data: doc,
-            });
-          }
-        }
-      })
-      .catch(console.error);
+  const deleteDoc = ID => {
+    sanityClient.delete({ query: `*[_type == "order" && id==${ID}` });
   };
 
-  return <button onClick={registerAllOrders}>register all orders</button>;
+  const fetchAllOrders = () => {
+    sanityClient.fetch(`*[_type == "order"]`).then(data => {
+      console.log(data);
+      setOrders(data);
+    });
+    //   for (const order of data) {
+    //     const doc = {
+    //       order_id: order.id,
+    //       price_in_cents: 0,
+    //       expected_delivery_datetime: newTime,
+    //     };
+
+    //     axios({
+    //       url: "http://localhost:5000/delivery",
+    //       method: "POST",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //       data: doc,
+    //     });
+    //   }
+    // })
+    // .catch(err => console.log("oops"));
+  };
+
+  const registerOrder = e => {
+    console.log(e.target.id);
+    const doc = {
+      order_id: e.target.id,
+      price_in_cents: 0,
+      expected_delivery_datetime: newTime,
+    };
+
+    console.log(doc);
+
+    axios({
+      url: "http://localhost:5000/delivery",
+      method: "post",
+      data: doc,
+    });
+  };
+
+  return (
+    <>
+      <button onClick={fetchAllOrders}>fetch all orders</button>
+      {orders.map(order => (
+        <button key={order.id} id={order.id} onClick={registerOrder}>
+          register this order {order.id}
+        </button>
+      ))}
+    </>
+  );
 };
 
 export default RegisterDelivery;
