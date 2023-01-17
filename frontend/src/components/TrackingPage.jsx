1e926e124c14882ee004ee01add09be5b6eaa528
DanielKnap <dctknap@gmail.com>
2023-01-17 01:06:26 +0100

    nothing

--- frontend/src/components/TrackingPage.jsx
+++ frontend/src/components/TrackingPage.jsx
@@ -12,12 +12,16 @@ const TrackingPage = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        sanityClient.fetch(`*[_type == "delivery" && id == ${trackingID}] {status}`)
+        sanityClient.fetch(`*[_type == "order" && id == (*[_type == "delivery" && id ==${trackingID}]){order_id}]{}`)
         .then(data => {
-            console.log(data[0]["status"])
-            setTrackingStatus(<p>{data[0]["status"]}</p>)
+            console.log(data)
         })
-        .catch(console.error)
+        // sanityClient.fetch(`*[_type == "delivery" && id == ${trackingID}] {status, expected_deliver_datetime}`)
+        // .then(data => {
+        //     console.log(data[0]["status"])
+        //     setTrackingStatus(<p>{data[0]["status"]}</p>)
+        // })
+        // .catch(console.error)
     }
 
     return (
